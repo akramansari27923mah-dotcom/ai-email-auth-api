7ba24aae7c10d67fbf131234e4432531f999d699
Akram Ansari <akramansari27923mah@gmail.com>
2026-03-25 05:47:11 -0400

    initial commit

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ const authMiddleware = (req, res, next) => {
     const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
 
     if (!token) {
-        res.status(401).json({
+       return res.status(401).json({
             success: false,
             message: 'Unauthorized'
         })
