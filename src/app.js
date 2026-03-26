3fb3420b0aeb4202c30ae963f6797b4c78262010
Akram Ansari <akramansari27923mah@gmail.com>
2026-03-26 07:59:51 -0400

    Changed Origin

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://ai-email-generater.vercel.app',
     credentials: true
 }))
 
